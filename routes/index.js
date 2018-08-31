1c962cf5c1e572758cde3d8474e1c590febc7ff3
qkrtjddlsgh <glgl2131@inha.edu>
2018-08-31 22:04:55 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/long', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   res.sendFile(__dirname + '/lending.html');
+});
+
+router.get('/short', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.sendFile(__dirname + '/lending_short.html');
 });
 
 module.exports = router;
